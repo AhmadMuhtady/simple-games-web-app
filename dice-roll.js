d1eaa6aaf7cf73c4a4a47aaeaa7548a3fa6fe768
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-12-24 22:50:13 +0200

    edited dom elements variables

--- dice-roll.js
+++ dice-roll.js
@@ -1,19 +1,19 @@
-const player1 = {
+const player0 = {
+	name: 'Player 1',
 	scoreDisplay: document.getElementById('score--0'),
 	currentDisplay: document.getElementById('current--0'),
 	element: document.querySelector('.player--1'),
 	currentScore: 0,
 	mainScore: 0,
-	isActive: true,
 };
 
-const player2 = {
+const player1 = {
+	name: 'Player 2 ',
 	scoreDisplay: document.getElementById('score--1'),
 	currentDisplay: document.getElementById('current--1'),
 	element: document.querySelector('.player--2'),
 	currentScore: 0,
 	mainScore: 0,
-	isActive: false,
 };
 
 const diceImage = document.querySelector('.dice');
@@ -22,15 +22,10 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 
+const modal = document.querySelector('.modal');
 const modalTitle = document.querySelector('.modalTitle');
-const modalDis = document.querySelector('.modalDis');
 const btnAgainModal = document.querySelector('.modalAgain');
 const btnCloseModal = document.querySelector('.close');
 
-const rollDice = () => {
-	return Math.floor(Math.random() * 6) + 1;
-};
-
-const changeImage = (number) => {
-	diceImage.src = `./images/dice-${number}.png`;
-};
+let gameFinished = false;
+let activePlayer = player0;
